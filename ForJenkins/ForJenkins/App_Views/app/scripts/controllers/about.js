adb2eb7e691c6add6f8e27a1645af14d34589383
leaf-nut <iyeon88@gmail.com>
2016-01-07 15:57:36 +0900

    app init

---
+++ ForJenkins/ForJenkins/App_Views/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jenkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jenkApp
+ */
+angular.module('jenkApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ForJenkins/ForJenkins/App_Views/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jenkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jenkApp
+ */
+angular.module('jenkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
