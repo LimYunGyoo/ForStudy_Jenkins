6add0880c6bbe0da3ed100581bb1ac33f2c19d0a
leaf-nut <iyeon88@gmail.com>
2016-01-14 14:24:27 +0900

    http add

--- ForJenkins/ForJenkins/App_Views/app/scripts/controllers/main.js
+++ ForJenkins/ForJenkins/App_Views/app/scripts/controllers/main.js
@@ -8,10 +8,20 @@
  * Controller of the jenkApp
  */
 angular.module('jenkApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http({
+        method: 'GET',
+        url: '/api/Text'
+    }).then(function successCallback(response) {
+        alert("res : " + response);
+    }, function errorCallback(response) {
+        alert("error!");
+    });
+    
   });
