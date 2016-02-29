bdfc80392b6a8b94a2d89de2e8ef2192e70eabe2
limyungyoo <koyahoo@naver.com>
2016-02-29 12:00:03 +0900

    add app

---
+++ ForJenkins/ForJenkins/App_Views/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jenkinApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jenkinApp
+ */
+angular.module('jenkinApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ForJenkins/ForJenkins/App_Views/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jenkinApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jenkinApp
+ */
+angular.module('jenkinApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
