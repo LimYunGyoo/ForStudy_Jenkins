74e906d34173dbb543d8fa79f36fdb776cb2963b
limyungyoo <koyahoo@naver.com>
2016-03-07 11:38:42 +0900

    Log4NetController add

---
+++ ForJenkins/ForJenkins/Controllers/Log4NetController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ForJenkins.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class Log4NetController : ApiController
+    {        
+        public IHttpActionResult GetText()
+        {
+            int result = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                result += i;
+            }
+
+            return Ok("result ok : " + result);
+        }
+
+
+
+    }
+}
